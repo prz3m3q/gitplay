7ef45081cf8923ee5d7a9dcc7f63c476d7e8b81b
przemek <prz3m3q@wp.pl>
2017-12-16 13:38:39 +0100

    gitignore

--- Hello.java
+++ Hello.java
@@ -1,7 +1,10 @@
 class Hello {
-	public private static void main(String[] args) {
-		System.out.println("witam");
+	public static void main(String[] args) {
+	    System.out.println("witam");
+	    System.out.println("witam");
 	}
 
-	public vod foo() {}
+	public void foo() {
+
+    };
 }
--- Hello2.java
+++ Hello2.java
@@ -1,5 +1,5 @@
-class Hello {
-	public private static void main(String[] args) {
+class Hello2 {
+	public static void main(String[] args) {
 		System.out.println("siema");	
 	}
 
